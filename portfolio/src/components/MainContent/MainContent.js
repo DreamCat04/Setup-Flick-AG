d3d6801bf163af00d0fb2605ddd0cd640955a12a
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-09 15:36:27 +0200

    Company name and slogan added

--- portfolio/src/components/MainContent/MainContent.js
+++ portfolio/src/components/MainContent/MainContent.js
@@ -1,6 +1,10 @@
 function MainContent(){
     return(
-        <p>Hi, I'm the main content yay!</p>
+        <div id="mainContent">
+        <h1>Setup-Flick</h1>
+        <h3>Ist Ihr PC kaputt oder funktioniert er nicht richtig? Bringen Sie ihn zu uns, wir reparieren ihn!</h3>
+        
+        </div>
     )
 }
 export default MainContent;
