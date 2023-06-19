72ab1acc30618dcdbc2c976f2dfe986f2c69e29c
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-19 20:37:47 +0200

    Scrapped tutorial, implemented Carousel from ChatGPT

---
+++ portfolio/src/components/ImageGallery/ImageCarousel.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const ImageCarousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleNextClick = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="image-carousel">
+      <button onClick={handlePrevClick}>Previous</button>
+      <img src={images[currentImageIndex]} alt="Carousel Image" />
+      <button onClick={handleNextClick}>Next</button>
+    </div>
+  );
+};
+
+export default ImageCarousel;
--- portfolio/src/components/MainContent/MainContent.js
+++ portfolio/src/components/MainContent/MainContent.js
@@ -1,72 +1,17 @@
 import { useState } from "react";
-import {
-    TouchableOpacity,
-    View,
-    Text,
-    Image,
-    FlatList,
-    Dimensions
-  } from 'react-native';
-  import Carousel, { Pagination } from 'react-native-snap-carousel';
+import ImageCarousel from "../ImageGallery/ImageCarousel";
+const images = [
+  'src/img/setup_broken.jpg',
+  'src/img/setup_fixed.jpg'
+  // Add more image URLs here
+];
 function MainContent(){
-const { width } = Dimensions.get('window');
-const SPACING = 10;
-const THUMB_SIZE = 80;
-
-const IMAGES = {
-  image1: require('../../img/setup_broken.jpg'),
-  image2: require('../../img/setup_fixed.jpg'),
-};
-const [images, setImages] = useState([
-    { id: '1', image: IMAGES.image1 },
-    { id: '2', image: IMAGES.image2 }
-  ]);
-  const [indexSelected, setIndexSelected] = useState(0);
-
-  const onSelect = indexSelected => {
-    setIndexSelected(indexSelected);
-  };
     return(
         <div id="mainContent">
         <h1>Setup-Flick</h1>
         <h3>Ist Ihr PC kaputt oder funktioniert er nicht richtig? Bringen Sie ihn zu uns, wir reparieren ihn!</h3>
-        <View style={{ flex: 1, backgroundColor: 'black', alignItems: 'center' }}>
-      <Text
-        style={{
-          color: 'white',
-          fontSize: 32,
-          marginTop: 50,
-          marginBottom: 25
-        }}
-      >
-      </Text>
-      <View style={{ flex: 1 / 2, marginTop: 20 }}>
-      <Carousel
-        layout='default'
-        data={images}
-        sliderWidth={width}
-        itemWidth={width}
-        renderItem={({ item, index }) => (
-          <Image
-            key={index}
-            style={{ width: '100%', height: '100%' }}
-            resizeMode='contain'
-            source={item.image}
-          />
-        )}
-        onSnapToItem={index => onSelect(index)}
-      />
-    </View>
-    <Pagination
-    inactiveDotColor='gray'
-    dotColor={'orange'}
-    activeDotIndex={indexSelected}
-    dotsLength={images.length}
-    animatedDuration={150}
-    inactiveDotScale={1}
-  />
-    </View>
+        <ImageCarousel images={images} />
         </div>
-    )
+    );
 }
 export default MainContent;
