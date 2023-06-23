f4358eced6a1c366ef766e71009ee6edb27e71cb
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-23 14:12:20 +0200

    Fix: gitignore now contains both drawio temporary file types

--- portfolio/src/pages/Offers.js
+++ portfolio/src/pages/Offers.js
@@ -1,7 +1,54 @@
+import { Carousel } from 'react-carousel-minimal';
+const data = [//This is sample data, replace with actual images once they're edited!
+    {
+      image: "https://thierry.kellyburger.com/imgSetup/setup_broken.jpg",
+      caption: "Wir machen dies.."
+    },
+    {
+      image: "https://thierry.kellyburger.com/imgSetup/setup_fixed.jpg",
+      caption: "..zu dem"
+    }
+  ];
+
+  const captionStyle = {
+    fontSize: '2em',
+    fontWeight: 'bold',
+  }
+  const slideNumberStyle = {
+    fontSize: '20px',
+    fontWeight: 'bold',
+  }
 function Offers(){
     return(
-        <div>
+        <div className="App">
             <h1>Angebote</h1>
+            <div style={{padding: "0 20px"}}>
+          <Carousel
+            data={data}
+            time={5000}
+            width="850px"
+            height="500px"
+            captionStyle={captionStyle}
+            radius="10px"
+            slideNumber={false}
+            slideNumberStyle={slideNumberStyle}
+            captionPosition="bottom"
+            automatic={true}
+            dots={true}
+            pauseIconColor="white"
+            pauseIconSize="40px"
+            slideBackgroundColor="darkgrey"
+            slideImageFit="cover"
+            thumbnails={true}
+            thumbnailWidth="100px"
+            style={{
+              textAlign: "center",
+              maxWidth: "850px",
+              maxHeight: "500px",
+              margin: "40px auto",
+            }}
+          />
+          </div>
         </div>
     );
 }
