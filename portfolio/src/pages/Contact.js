e43ce2ea998f266ddae8418725ec13a62d75c499
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-23 21:42:28 +0200

    Added contact form for contact page

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,8 +1,20 @@
 function Contact(){
     return(
-        <div>
-            <p>Kontakt</p>
+        <div className="App">
+            <h1>Kontakt</h1>
+            <form action={submit()}>
+                <label for="name">Name</label>
+                <input name="name" required="true"></input>
+                <label for="email">E-Mail</label>
+                <input name="email" type="email" required="true"></input>
+                <label for="message">Ihre Nachricht</label>
+                <input name="message"></input>
+                <button type="submit">Absenden</button>
+            </form>
         </div>
     );
+}
+function submit(){
+
 }
 export default Contact;
