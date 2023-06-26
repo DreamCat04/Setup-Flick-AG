e07995cf3d7a519a8ff3ef4a2a263dd22ff4a813
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-26 10:41:09 +0200

    Add about us

--- portfolio/src/pages/AboutMe.js
+++ portfolio/src/pages/AboutMe.js
@@ -2,6 +2,55 @@ function AboutMe(){
     return(
         <div className="App">
             <h1>Über Uns</h1>
+            <p>Über uns:
+            Die Setup-Flick AG ist ein renommiertes Unternehmen in der Schweiz, 
+            das sich auf die Reparatur von Computern und Computerzubehör spezialisiert hat. 
+            Seit unserer Gründung vor mehreren Jahren haben wir uns einen 
+            hervorragenden Ruf für unsere professionellen und zuverlässigen 
+            Dienstleistungen erarbeitet.<br/>
+
+            Unser erfahrenes Team besteht aus hochqualifizierten Technikern, 
+            die über umfangreiches Know-how in der Reparatur und Wartung von 
+            Computern, Laptops, Druckern, Monitoren und anderen Computerzubehör 
+            verfügen. Wir verstehen, wie wichtig Ihre Geräte für Sie sind, 
+            sei es für den privaten oder geschäftlichen Gebrauch, und wir 
+            setzen unser Fachwissen ein, um eine schnelle und effiziente 
+            Reparatur zu gewährleisten.<br/>
+
+            Unsere Dienstleistungen umfassen:<br/>
+
+            1. Computerreparatur: Wir bieten umfassende Reparaturdienstleistungen für 
+            Computer und Laptops an. Egal, ob es sich um Hardwareprobleme, Softwarefehler, 
+            Virenbefall oder andere technische Probleme handelt, unsere Experten finden die 
+            richtige Lösung, um Ihr Gerät wieder zum Laufen zu bringen.<br/>
+
+            2. Druckerreparatur: Wenn Ihr Drucker streikt oder Qualitätsprobleme aufweist, 
+            sind wir zur Stelle. Unsere Techniker sind erfahren in der Reparatur verschiedener 
+            Druckermarken und -modelle und können häufig auftretende Probleme wie Papierstaus, 
+            Tintenverstopfungen oder Fehlermeldungen beheben.<br/>
+
+            3. Monitorreparatur: Defekte Bildschirme können frustrierend sein, 
+            aber wir haben das Wissen und die Fähigkeiten, um sie wieder zum 
+            Funktionieren zu bringen. Ob es sich um Pixelfehler, Flackern, 
+            keine Anzeige oder andere Probleme handelt, wir bieten eine 
+            schnelle und effektive Monitorreparatur an.<br/>
+
+            4. Aufrüstung und Wartung: Neben Reparaturen bieten wir auch Aufrüstungs- 
+            und Wartungsdienste an, um sicherzustellen, dass Ihre Computer und 
+            Zubehörteile optimal funktionieren. Wir helfen Ihnen bei der Installation 
+            von Hardwarekomponenten, der Aktualisierung von Software, der Durchführung 
+            von Systemoptimierungen und vielem mehr.<br/><br/>
+
+            Bei der Setup-Flick AG legen wir großen Wert auf exzellenten Kundenservice. 
+            Wir verstehen, dass Computerprobleme Ihre Produktivität beeinträchtigen können, 
+            daher arbeiten wir zügig, um Ihre Geräte so schnell wie möglich wieder in 
+            Betrieb zu nehmen. Wir verwenden hochwertige Ersatzteile und führen 
+            gründliche Tests durch, um sicherzustellen, dass Ihre Geräte optimal funktionieren.<br/><br/>
+
+            Kontaktieren Sie uns noch heute, um einen Termin für eine Reparatur oder Wartung 
+            zu vereinbaren. Unser freundliches und kompetentes Team steht Ihnen gerne zur 
+            Verfügung und freut sich darauf, Ihnen bei der Lösung Ihrer Computerprobleme zu helfen.
+            </p>
         </div>
     );
 }
