c46f5c58a2e7fbc337b613bbb24e107ddb14a5ee
DreamCat04 <thierry@kellyburger.com>
2023-06-27 08:35:38 +0200

    Implement background colors for sections

--- portfolio/src/pages/LandingPage.js
+++ portfolio/src/pages/LandingPage.js
@@ -26,9 +26,15 @@ function LandingPage(){
       <section id='landPageMain'>
           <div id='leftContainer'>
             <h1>Setup-Flick AG</h1>
-            <h3>Ist Ihr PC kaputt oder funktioniert er nicht richtig? Bringen Sie ihn zu uns, wir reparieren ihn!</h3>
-            <p>Setup-Flick AG repariert alles in einem Computer und auch rund um den Computer. Wir beherrschen Microsoldering, 
-              Reflowing von Chips und vieles mehr</p>
+            <h3>Suchen Sie nach einer schnellen und professionellen Lösung für Ihre Computerprobleme?
+              Sind Sie frustriert über den defekten Drucker oder den fehlerhaften Monitor?
+              Bei Setup-Flick AG sind wir darauf spezialisiert, Ihre Computer und Computerzubehör
+              wieder zum Laufen zu bringen. Unser erfahrenes Team von Experten steht bereit,
+              um Ihnen bei allen Arten von technischen Problemen zu helfen. Egal, ob es sich um
+              Hardware- oder Softwareprobleme handelt, wir haben das Know-how und die Fähigkeiten,
+              um eine schnelle und effiziente Reparatur durchzuführen. Wir verstehen, wie wichtig
+              Ihre Geräte für Sie sind und arbeiten mit höchster Sorgfalt, um eine optimale Leistung
+              und Haltbarkeit zu gewährleisten.</h3>
           </div>
           <div id='rightContainer'>
             <div style={{padding: "0 20px"}}>
@@ -61,8 +67,12 @@ function LandingPage(){
           </div>{/*For right side container*/}
       </section>
       <section className='landPageAdv'>
+      <h3>Was können wir für Sie tun?</h3>
+      </section>
+      <section className='landPageAdv'>
         <div className='advLeft1'>
-          <img src='https://thierry.kellyburger.com/imgSetup/PC_aufschrauben.png' className='offerImg'></img>
+          <img src='https://thierry.kellyburger.com/imgSetup/PC_aufschrauben.png' className='offerImg'
+          alt='Person, die einen Computer aufschraubt'></img>
         </div>
         <div className='advRight1'>
           <h2>Computerreparatur</h2>
@@ -79,7 +89,33 @@ function LandingPage(){
             Wir beheben Ihr Problem schnell, sodass Sie nicht lange auf Ihr Gerät warten müssen.</p>
         </div>
         <div className='advRight2'>
-          <img src='https://thierry.kellyburger.com/imgSetup/Grafikkarte_von_oben.png' className='offerImg'></img>
+          <img src='https://thierry.kellyburger.com/imgSetup/Grafikkarte_von_oben.png' className='offerImg'
+          alt='Grafikkarte von oben'></img>
+        </div>
+      </section>
+      <section className='landPageAdv'>
+        <div className='advLeft1'>
+          <img src='https://thierry.kellyburger.com/imgSetup/Tablet_kaputt_front_view.png' className='offerImg'
+          alt='Computersetup mit kaputten Bildschirmen'></img>
+        </div>
+        <div className='advRight1'>
+          <h2>Monitorreparatur</h2>
+          <p>Defekte Bildschirme können Ihre Arbeit behindern.
+            Unsere Experten analysieren und beheben Probleme wie Pixelfehler, 
+            Flackern oder fehlende Anzeige, um Ihre Monitore wieder in Topform zu bringen.
+            Auch Brüche im Display sind kein Problem für uns.</p>
+        </div>
+      </section>
+      <section className='landPageAdv'>
+        <div className='advLeft2'>
+          <h2>Aufrüstung&Wartung</h2>
+          <p>Wir bieten auch Dienstleistungen zur Aufrüstung und Wartung an, um sicherzustellen, 
+            dass Ihre Geräte optimal funktionieren. Von der Installation neuer Hardwarekomponenten 
+            bis hin zur Optimierung der Software stehen wir Ihnen zur Seite.</p>
+        </div>
+        <div className='advRight2'>
+          <img src='https://thierry.kellyburger.com/imgSetup/Haufen_von_Teilen.png' className='offerImg'
+          alt='Ein Haufen von Computerteilen'></img>
         </div>
       </section>
     </div>//For main div
