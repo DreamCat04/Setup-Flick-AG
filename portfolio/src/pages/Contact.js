f86e16cc5c51bd321c6a9f324259e569a6591066
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-28 19:38:32 +0200

    Properly stlye form

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -49,20 +49,22 @@ function Contact(){
                 Reparaturanfrage stellen oder möchten sie uns Feedback geben?
                 Kontaktieren Sie uns über das untenstehende Formular!</p>
             <form onSubmit={handleSubmit}>
-                <label for="name">Name</label>
-                <input name="name" required={true} onChange={(e) => setName(e.target.value)}/>
-                <label for="email">E-Mail</label><br/>
+                <label for="name" className="formLabel">Name</label><br/>
+                <input name="name" required={true} onChange={(e) => setName(e.target.value)}/><br/>
+                <label for="email" className="formLabel">E-Mail</label><br/>
                 <input name="email" type="email" required={true} value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <p id="formLabel">Grund Ihrer Anfrage</p>
-                <label for="support">Supportanfrage</label>
+                <h4 className="formLabel">Grund Ihrer Anfrage</h4>
+                <div id="radioDiv">
+                <label for="support" className="radioLabel">Supportanfrage</label>
                 <input type="radio" name="support" value="Support request" checked={requestTpye === 'Support request'} onChange={(e) => setRequestType(e.target.value)}></input>
-                <label for="repairRequest">Reparaturanfrage</label>
+                <label for="repairRequest" className="radioLabel">Reparaturanfrage</label>
                 <input type="radio" name="repairRequest" value="Repair request" checked={requestTpye === 'Repair request'} onChange={(e) => setRequestType(e.target.value)}></input>
-                <label for="support">Frage</label>
+                <label for="question" className="radioLabel">Frage</label>
                 <input type="radio" name="question" value="Question" checked={requestTpye === 'Question'} onChange={(e) => setRequestType(e.target.value)}></input>
-                <label for="repairRequest">Feedback</label>
-                <input type="radio" name="feedback" value="Feedback" checked={requestTpye === 'Feedback'} onChange={(e) => setRequestType(e.target.value)}></input>
-                <label for="message">Ihre Nachricht</label>
+                <label for="repairRequest" className="radioLabel">Feedback</label>
+                <input type="radio" name="feedback" value="Feedback" checked={requestTpye === 'Feedback'} onChange={(e) => setRequestType(e.target.value)} style={{"margin-bottom": "20px"}}></input>
+                </div>
+                <label for="message" className="formLabel">Ihre Nachricht</label><br/>
                 <textarea name="message" value={message} onChange={(e) => setMessage(e.target.value)}/><br/>
                 <button type="submit">Absenden</button>
             </form>
